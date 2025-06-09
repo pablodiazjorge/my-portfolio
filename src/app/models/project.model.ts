fc59ad599fe557a491976ffa0cc234f7d8dc01df
pablodiazjorge <pablodiazjorgegarcia@gmail.com>
2025-06-09 18:04:07 +0200

    feat: updated conditions projects display

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -3,5 +3,7 @@ export interface Project {
   PROJECTS_DESCRIPTION: string;
   PROJECTS_IMAGE: string;
   PROJECTS_LINK: string;
+  PROJECTS_DEMO: string;
+  PROJECTS_DEMO_ACTIVE: string;
   PROJECTS_TECNOLOGIES: string[];
 }
